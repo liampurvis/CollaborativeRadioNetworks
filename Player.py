07c0ef91beec54c00aeefefe61287f717e9a5801
Liam Purvis <purvis.liam@berkeley.edu>
2019-02-19 15:52:52 -0800

    adding update location function

--- Player.py
+++ Player.py
@@ -14,16 +14,22 @@ class Player:
     power = 1 #power of transmission
     min_frequency = 1000 #represents the channel
     max_frequency = 1010 #represents the channel
-    
+
     def __init__(self, t_x, t_y, r_x, r_y):
         self.t_x = t_x
         self.t_y = t_y
         self.r_x = r_x
         self.r_y = r_y
-    
+
     def set_channel(self, min_f, max_f):
         self.min_frequency = min_f
         self.max_frequency = max_f
-    
+
     def next_step(self, success): #to overwrite depending on the algorithm
         pass
+
+    def update_positions(self, new_t_x, new_t_y, new_r_x, new_r_y):
+      t_x = new_t_x
+      t_y = new_t_y
+      r_x = new_r_x
+      r_y = new_r_y
