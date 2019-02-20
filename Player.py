8823f24355eca206174740884e559b9beb8394b1
Alexandre Brenelliere <a.brenelliere@gmail.com>
2019-02-19 16:38:07 -0800

    Changing definition of channel / min max frequency

--- Player.py
+++ Player.py
@@ -12,26 +12,21 @@ class Player:
     r_y=1 #position of the receiver
 
     power = 1 #power of transmission
+    central_frequency= 1005
+    channel_width = 5
+    
     min_frequency = 1000 #represents the channel
-    max_frequency = 1010 #represents the channel
-
-
-
+    max_frequency = 1100 #represents the channel
+    
     def __init__(self, t_x, t_y, r_x, r_y):
         self.t_x = t_x
         self.t_y = t_y
         self.r_x = r_x
         self.r_y = r_y
-
-    def set_channel(self, min_f, max_f):
-        self.min_frequency = min_f
-        self.max_frequency = max_f
-
+    
+    def set_channel(self, central, width):
+        self.central_frequency = central
+        self.channel_width = width
+    
     def next_step(self, success): #to overwrite depending on the algorithm
         pass
-
-    def update_positions(self, new_t_x, new_t_y, new_r_x, new_r_y):
-      t_x = new_t_x
-      t_y = new_t_y
-      r_x = new_r_x
-      r_y = new_r_y
--- core.py
+++ core.py
@@ -5,10 +5,6 @@ Created on Sat Feb 16 14:59:06 2019
 @author: Alexandre
 """
 
-# LIAM'S COMMENT: for loops are quite slow in python.
-# As we scale up we may need to revisit this.
-# Let's ignore for now.
-
 class env_core:
     """
         This class simulates the environment core
@@ -16,28 +12,27 @@ class env_core:
     players = [] #list of objects of class Player
     NB_PLAYERS = 0 #number of players = len(players)
     NB_STEPS = 100 #number of steps to play
-
+    
     SNR_THRESHOLD = 1 #min SNR for success
-
+    
     # initialize the core for a given set of players
     def __init__(self, players, nb_steps=100):
         self.players = players
         self.NB_PLAYERS = len(players)
         self.NB_STEPS = nb_steps
-
+    
     def run_simulation(self):
         for i in range(self.NB_STEPS):
             self.next_step()
-
-
-    # computes the success rate for every player and asks for next step
+            
+    # computes the success rate for every player and asks for next step    
     def next_step(self):
         success = self.computeSuccess()
         print(success) #logging the results
         for i in range(self.NB_PLAYERS):
             self.players[i].next_step(success[i])
-
-    # from a given set of settings, compute the success rate of each player
+    
+    # from a given set of settings, compute the success rate of each player        
     def computeSuccess(self):
         success = []
         for i in range(self.NB_PLAYERS):
@@ -46,7 +41,7 @@ class env_core:
             for j in range(self.NB_PLAYERS):
                 if j!=i:
                     noise += self.players[j].power*self.overlap(j, i) / self.distSquare(j, i)
-
+            
             # Success is determine by the comparison of SNR to a threshold
             if noise==0:
                 success.append(1)
@@ -57,17 +52,22 @@ class env_core:
                 else:
                     success.append(0)
         return success
-
+    
     # square of the distance between transmitter i and receiver j
     def distSquare(self, i, j):
         return (self.players[i].t_x-self.players[j].r_x)**2 + (self.players[i].t_y-self.players[j].r_y)**2
-
+    
     # how much of his total power transmitter i is overlapping on receiver j
     def overlap(self, i, j):
-        M = min(self.players[i].max_frequency, self.players[j].max_frequency)
-        m = max(self.players[i].min_frequency, self.players[j].min_frequency)
-
-        M = max(M, self.players[j].min_frequency)
-        m = min(m, self.players[j].max_frequency)
-
+        m_i = self.players[i].central_frequency-self.players[i].channel_width
+        M_i = self.players[i].central_frequency+self.players[i].channel_width
+        m_j = self.players[j].central_frequency-self.players[j].channel_width
+        M_j = self.players[j].central_frequency+self.players[j].channel_width
+        
+        M = min(M_i, M_j)
+        m = max(m_i, m_j)
+        
+        M = max(M, m_j)
+        m = min(m, M_j)
+        
         return (M-m)/(self.players[i].max_frequency - self.players[i].min_frequency)
