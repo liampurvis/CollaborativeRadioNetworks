d52154f5c4ab41c343f09602151e228682344544
yilun_tang <yilun_tang@berkeley.edu>
2019-02-21 23:47:54 -0800

    working on adding naive method of CSMA for vanilla player

--- Player.py
+++ Player.py
@@ -29,15 +29,16 @@ class Player:
         self.t_y = t_y
         self.r_x = r_x
         self.r_y = r_y
+        self.available_strategy = {}
     
     def set_channel(self, central, width):
         self.central_frequency = central
         self.channel_width = width
         
-        self.blocker_counter = 5      
+        self.blocker_counter = 5    
     
     def next_step(self, success): #to overwrite depending on the algorithm
-        self.previous_settings.append(success)
+        self.previous_successes.append(success)
         self.save_setting()
         self.blocker_counter -= 1
         
@@ -46,6 +47,61 @@ class Player:
         # if success < 0, it means that the player was listening. Success value 
         # corresponds to the current level of noise observed on the channel
         # in the < 0, success is NOT a reward. It can just be used for CSMA
+        if self.blocker_counter <= 0:
+            if success >= 0:
+                # determine if want to change setting or not
+                #change_setting( etc )
+                pass
+            else:
+                # determine if want to change setting or not
+                pass
+            return 1  # dummy return value to tell outside environment if we are acting this step or not
+        else:
+            return 0
     
     def save_setting(self):
         self.previous_settings.append((self.power, self.central_frequency, self.channel_width))
+
+    def change_setting(self, new_power, new_central_frequency, new_channel_width):
+        #
+        save_setting()
+        self.blocker_counter = 5  # reset internal counter
+        self.power = new_power
+        self.central_frequency = new_central_frequency
+        self.channel_width = new_channel_width
+
+    def CSMA_init(self, CSMA_mode, waiting_time_center, max_random_variance):
+        CSMA_config(CSMA_mode, waiting_time_center, max_random_variance)
+        self.available_strategy.append("CSMA")
+        pass
+
+    def CSMA_config(self,  CSMA_mode, waiting_time_center, max_random_variance):
+        self.
+        pass
+
+    def UCB_init():
+        UCB_config()
+        self.available_strategy.append("UCB")
+        pass
+
+    def UCB_config():
+        pass
+
+    def Tompson_init():
+        Tompson_config()
+        self.available_strategy.append("Tompson")
+        pass
+
+    def Tompson_config():
+        pass
+
+
+
+
+
+
+
+
+
+
+
