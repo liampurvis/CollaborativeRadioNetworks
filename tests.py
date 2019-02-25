794d8706239c76c26dbb476e747f325889d8f867
Liam Purvis <purvis.liam@berkeley.edu>
2019-02-25 13:32:18 -0800

    test and location test not in different files

--- tests.py
+++ tests.py
@@ -11,31 +11,16 @@ from core import env_core
 import matplotlib.pyplot as plt
 import numpy as np
 
-test_random_walk = True
-# if we are testing random walk
-if test_random_walk:
-    p1 = Random(id = -666, t_x = 0, t_y = 0, r_x = 1, r_y = 1, prob = .5, random_walk = True)
-    p2 = Random(id = 420, t_x = -1, t_y = -1, r_x = 1, r_y = 1, prob = .5)
-    env = env_core([p1, p2])
-    env.run_simulation(1000)
-    t_walk = np.array(p1.previous_t_positions)
-    r_walk = np.array(p1.previous_r_positions)
-    plt.plot(t_walk[:, 0], t_walk[:, 1], label="transmitter")
-    plt.plot(r_walk[:, 0], r_walk[:, 1], label= "reciever")
-    plt.legend(loc='upper left')
-    plt.title("Random Walk of Transmitter and Reciever")
-    plt.ylabel("Longitude")
-    plt.xlabel("Latitude")
-    plt.show()
 
-else:
-    p1 = Player(-1,1,1,-1)
-    p2 = Random(-1,-1,1,1, .5)
 
-    env = env_core([p1, p2])
 
-    print("First steps")
-    env.run_simulation(5)
-    env.players[1].set_channel(1060,1070)
-    print("After changing settings")
-    env.run_simulation(20)
+  p1 = Player(-1,1,1,-1)
+  p2 = Random(-1,-1,1,1, .5)
+
+  env = env_core([p1, p2])
+
+  print("First steps")
+  env.run_simulation(5)
+  env.players[1].set_channel(1060,1070)
+  print("After changing settings")
+  env.run_simulation(20)
