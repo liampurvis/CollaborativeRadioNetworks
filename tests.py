faaa99e6e501a0f39bbcfc9c2463fd7394303c9a
Alexandre Brenelliere <a.brenelliere@gmail.com>
2019-02-25 13:34:33 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -17,10 +17,10 @@ import numpy as np
   p1 = Player(-1,1,1,-1)
   p2 = Random(-1,-1,1,1, .5)
 
-  env = env_core([p1, p2])
+  env = env_core([p1, p2], time_refs = [2,6])
 
   print("First steps")
   env.run_simulation(5)
-  env.players[1].set_channel(1060,1070)
+  env.players[1].set_channel(1060,5)
   print("After changing settings")
   env.run_simulation(20)
