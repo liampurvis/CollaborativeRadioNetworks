1f61ed7f12d3e863a44097fa753d8de589287aa1
yilun_tang <yilun_tang@berkeley.edu>
2019-04-22 20:58:05 -0700

    modified to run longer tests

--- demo_csma.py
+++ demo_csma.py
@@ -29,8 +29,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 p1 = Player(0,-1,0,1,0)
-p2 = CSMA(1,1.01,0,-1.01,0, 0.3, 0, 1.0)
-p3 = CSMA(2,0.99,0,-1.01,0, 0.3, 3, 0.5)
+p2 = CSMA(1,1.01,0,-1.01,0, 0.25, 5, 0.9)
+p3 = CSMA(2,0.99,0,-1.01,0, 0.25, 5, 0.9)
 # p4 = CSMA(3,1.02,0,-1.01,0, 0.5, 0, 0.5)
 
 # env = env_core([p1, p2], time_refs = [2,4])
@@ -41,7 +41,7 @@ env.run_simulation(5)
 # env.run_simulation(10)
 env.players[0].set_channel(1060,5)
 env.run_simulation(10)
-env.players[1].set_channel(1060,5)
-env.run_simulation(20)
+# env.players[1].set_channel(1060,5)
+env.run_simulation(400)
 
 env.displayResults(figsize = (10,10))
